e44d43d47fd14a2badadb3532ee26b6857d7a853
Jose Marino <jmarino@rackspace.com>
2014-11-20 10:37:36 -0600

    added two examples to show my preferred formatting for API calls

---
+++ Examples.ps1
@@ -0,0 +1,20 @@
+﻿# example no body
+
+$Region= "Regioninfo"
+
+$URI = (($Endpoint.access.serviceCatalog | Where-Object name -eq "cloudServersOpenStack").endpoints | Where-Object region -eq $Region).publicURL + "/servers"
+$execute = Invoke-RestMethod -URI $URI -Method Get -Headers $AuthToken
+$execute | Format-Wide  #this just shows the results
+
+
+# Example with Body
+
+
+$Var1 = "Var1info"
+$Var2 = "Var2info"
+$Var3 = "Var3info"
+$Region= "Regioninfo"
+
+$Body = @{"FromDoc" = @{"Param1" = $Var1; "Param2" = $Var2 ; "Param2" = $Var3 }} | convertTo-Json
+$URI = (($Endpoint.access.serviceCatalog | Where-Object name -eq "cloudServersOpenStack").endpoints | Where-Object region -eq $Region).publicURL + "/servers"
+$execute = Invoke-RestMethod -Uri $URI -Method POST -Headers $AuthToken -Body $CBody -ContentType "application/json"
